964273ca15e50117938fed78c9d45e56d005d95e
salman1256@yahoo.com <salman1256@yahoo.com>
2022-02-25 14:39:17 +0530

    After Adding Customers

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppMVC.Models;
 
 namespace WebAppMVC.Controllers
 {
@@ -13,5 +14,18 @@ namespace WebAppMVC.Controllers
         {
             return View();
         }
+        public ActionResult CustomerList()
+        {
+            List<Customer> listCust = new List<Customer>
+            {
+             new Customer{ CId=1,CName="Sam",CCity="Delhi",CODLimit=500000.90},
+             new Customer{ CId=1,CName="Neha",CCity="Mumbai",CODLimit=450000.90},
+              new Customer{ CId=4,CName="Vipul",CCity="Pune",CODLimit=900000.90},
+               new Customer{ CId=5,CName="Nitu",CCity="Goa",CODLimit=800000.90},
+                new Customer{ CId=8,CName="Raj",CCity="Delhi",CODLimit=900000.90},
+            };
+            return View(listCust);
+        }
+        
     }
 }
---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC.Models
+{
+    public class Customer
+    {
+        public int CId { get; set; }
+        public string CName { get; set; }
+        public string CCity { get; set; }
+        public double CODLimit { get; set; }
+       
+    }
+}
