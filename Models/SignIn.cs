56569db9262072a4c3dbea6e142cd40a6fe16278
salman1256@yahoo.com <salman1256@yahoo.com>
2022-02-25 15:01:38 +0530

    SignIn Changes

---
+++ Models/SignIn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC.Models
+{
+    public class SignIn
+    {    [Display(Name ="User Name")]
+         [DataType(DataType.Text)]
+         [Required]
+        public string UserName { get; set; }
+        [Display(Name = "User Password")]
+        [DataType(DataType.Password)]
+        [Required]
+        public string  UserPwd { get; set; }
+    }
+}
